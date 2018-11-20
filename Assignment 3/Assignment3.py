a6ba0ff78e9847236ea565fc42eb4fde26cfdbbe
prashanth <prashanthkanduri66@gmail.com>
2018-11-20 17:33:30 -0500

    Algorithm update. Added code for plotting input vs running time

--- Assignment 3/Assignment3.py
+++ Assignment 3/Assignment3.py
@@ -2,10 +2,12 @@ import math
 import numpy as np
 import random
 from random import randint
+import time
+import matplotlib.pyplot as plt
 
 
 #n is the number of shifts
-n=30
+
 dic={}
 
 def testCases(n):
@@ -14,8 +16,8 @@ def testCases(n):
 
 	for i in range(1,n+1):
 
-		start=randint(1,120)
-		end=start+5
+		start=randint(1,2*(n))
+		end=start+4
 
 		dic[i]=[start,end]
 
@@ -25,23 +27,9 @@ def testCases(n):
 
 
 
-# intervals=[]
-
-# for i in dic.values():
-# 	intervals.append(i)
-
-# #print(intervals)
-
-# sortedIntervals=sorted(intervals, key=lambda x: (x[1]))
-# #print(sortedIntervals)
-
-# committeeCount=0
-# temp=sortedIntervals
-
-# supervisoryCommittee=[]
 
 
-def main():
+def main(n):
 
 	dic=testCases(n)
 	intervals=[]
@@ -53,8 +41,7 @@ def main():
 
 	sortedIntervals=sorted(intervals, key=lambda x: (x[1]))
 
-	#sortedIntervals=[[1, 6], [3, 8], [11, 16], [12, 17], [17, 22], [17, 22], [17, 22], [21, 26], [25, 30], [25, 30]]
-	print(sortedIntervals)
+	# print(sortedIntervals)
 
 	committeeCount=0
 	dupe=sortedIntervals
@@ -88,14 +75,35 @@ def main():
 
 			a+=len(temp)+1
 
-
+	print("length of supervisoryCommittee is "+str(len(supervisoryCommittee)))
 	return supervisoryCommittee
 
 
+timeForVarInput=[]
+
+for i in range(1,1001):
 
 
-for i in range(1000):
 
-	print(i)
-	print(main())
+	start=time.time()
+	print("for input size "+str(i))
+	#print(main(i))
+	main(i)	
+	end=time.time()
+	if i%100==0:
 
+		timeForVarInput.append(end-start)
+
+
+print(timeForVarInput)
+
+
+
+fig = plt.figure(2)
+plt.plot(np.arange(0, 1000,100), timeForVarInput, color = 'blue')
+
+plt.xticks(np.arange(0, 1000,100))
+plt.xlabel('Input size', fontsize=14)
+plt.ylabel('Running Time', fontsize=14)
+fig.savefig('Input size vs Running time.png')
+plt.show()
